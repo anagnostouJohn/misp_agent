2ca252636a23d1d24ec687b37fc52db8e8498b5f
John Anagnostou <anagnostou_j@yahoo.com>
2018-07-18 20:00:50 +0000

    Add files via upload

--- keys.py
+++ keys.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-misp_url = 'https://192.168.168.254/'
-misp_key = 'LmJRokjX1DELFKDR7GRP86HBsxfA4kkBDO6sqaHs' # The MISP auth key can be found on the MISP web interface under the automation section
+#misp_url = 'https://192.168.168.254/'
+misp_url = 'https://192.168.1.5/'
+#misp_key = 'LmJRokjX1DELFKDR7GRP86HBsxfA4kkBDO6sqaHs' # The MISP auth key can be found on the MISP web interface under the automation section
+misp_key = 'AOp44we6euKRKyIFvF0bBiWlO2ulsFnfU1tS9wzD' # The MISP auth key can be found on the MISP web interface under the automation section
+
 misp_verifycert = True
