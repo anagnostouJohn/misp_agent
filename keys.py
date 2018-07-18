f93919cd05d4a47493f405e60d832f8ec645ceb5
anagnostouJohn <38450964+anagnostouJohn@users.noreply.github.com>
2018-07-18 14:49:15 +0300

    Add files via upload

--- keys.py
+++ keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-misp_url = 'http://192.168.168.60/'
-misp_key = 'zproiNsOvoU8v22bJxYV58nY0Pvfeg0nbc99B9sW' # The MISP auth key can be found on the MISP web interface under the automation section
+misp_url = 'https://192.168.168.254/'
+misp_key = 'LmJRokjX1DELFKDR7GRP86HBsxfA4kkBDO6sqaHs' # The MISP auth key can be found on the MISP web interface under the automation section
 misp_verifycert = True
--- server.py
+++ server.py
@@ -1,13 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 from keys import misp_url, misp_key
 from pymisp import PyMISP
 import socket
 import ssl
 import os
+#import sys
+import yara
+import re
+import fileinput
 
 KEYFILE = os.getcwd()+'/certs/key.pem'
 CERTFILE = os.getcwd()+'/certs/cert.pem'
+
+def check_yara(path):
+    try:
+        x = yara.compile(path)
+        print("OK", x)
+        x.save(os.getcwd()+"/yara_files/saved_file.yara")
+        return
+    except yara.SyntaxError as err:
+        print(err)
+        f = err.args[0]
+        #n = re.search(r'\((.*?)\)',f).group(1)#Eyresh kati pou einai anamesa se ()
+        z = re.findall(r'"([^"]*)"', f) #Eyresh kati pou einai anamesa se ""
+        counter = 0
+        with fileinput.FileInput(path, inplace=True) as file:
+            for line in file:
+                print(line.replace(z[0], (z[0]+str(counter))), end='')                
+                counter+=1
+        check_yara(path)
+
+
 
 def init(url, key):
     return PyMISP(url, key, False, 'json', debug=False)
@@ -21,7 +46,9 @@ def echo_client(s):
         print (type(agent), type(a)) 
         agent.send(b'OK')
         x = input("Enter Something: ")
+        print(x)
         agent.close()
+        
         
         
         #data = s.recv(8192)
@@ -62,13 +89,27 @@ def echo_server(address):
 if __name__ == '__main__':
     
 
-    ssl_soc = echo_server((socket.gethostbyname('localhost'), 8089))
+    #ssl_soc = echo_server((socket.gethostbyname('localhost'), 8089))
     misp = init(misp_url, misp_key)
     result_ip_src= misp.get_all_attributes_txt(type_attr="ip-src")
     result_ip_dest = misp.get_all_attributes_txt(type_attr="ip-dst")
     result_yara= misp.get_all_attributes_txt(type_attr="yara")
-    print("SRC : ", result_ip_src.text, "DEST : ", result_ip_dest.text,  "YARA : ", result_yara.text)
-    print("OK")
+    path = os.getcwd()+"/yara_files/yara_file.yara"
+    print(path)
+    file_new = open(path, "w")
     
-    while True:
-        echo_client(ssl_soc)
+    file_new.write(result_yara.text)
+    file_new.close()
+    check_yara(path)
+    hh = yara.load(os.getcwd()+"/yara_files/saved_file.yara")
+    print(f"END {hh}")
+    #print("SRC : ", result_ip_src.text, "DEST : ", result_ip_dest.text,  "YARA : ", result_yara.text)
+
+    
+    #while True:
+    #    echo_client(ssl_soc)
+
+
+
+
+
