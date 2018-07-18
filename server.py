ae48e483fc066f457d5b3d70ac0e09283a4fd297
John Anagnostou <anagnostou_j@yahoo.com>
2018-07-18 20:00:32 +0000

    Add files via upload

--- server.py
+++ server.py
@@ -10,29 +10,53 @@ import os
 import yara
 import re
 import fileinput
+import logging
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
 
 KEYFILE = os.getcwd()+'/certs/key.pem'
 CERTFILE = os.getcwd()+'/certs/cert.pem'
 
-def check_yara(path):
+
+def check_wrong_yara(path):
+    book = open(path, "r")
+    all_lines = book.read()
+    f = all_lines.split("rule")
+    for i in reversed(f):
+        print(i)
+        try:
+            yara.compile(source="rule " + i)
+        except Exception as err:
+            #logging.error('Error in Yara File : ' + str(err) + " Yara File : " + i + "\n")
+            print(err)
+            f.remove(i)
+
+    book.close()
+    file = open(os.getcwd()+"/yara_files/clean.yara", "w")
+
+    for i in f:
+        print(">>>>>>>>>>>>>",i,"<<<<<<<<<<<<<<<<<<<<")
+        file.write("rule " + i + " \n")
+    file.close()
+
+
+def check_double_yara(path):
     try:
-        x = yara.compile(path)
-        print("OK", x)
-        x.save(os.getcwd()+"/yara_files/saved_file.yara")
+        yara.compile(path)
         return
     except yara.SyntaxError as err:
-        print(err)
         f = err.args[0]
-        #n = re.search(r'\((.*?)\)',f).group(1)#Eyresh kati pou einai anamesa se ()
-        z = re.findall(r'"([^"]*)"', f) #Eyresh kati pou einai anamesa se ""
+        # n = re.search(r'\((.*?)\)',f).group(1)#Eyresh kati pou einai anamesa se ()
+        z = re.findall(r'"([^"]*)"', f)  # Eyresh kati pou einai anamesa se ""
         counter = 0
         with fileinput.FileInput(path, inplace=True) as file:
             for line in file:
-                print(line.replace(z[0], (z[0]+str(counter))), end='')                
-                counter+=1
-        check_yara(path)
-
-
+                print(line.replace(z[0], (z[0] + str(counter))), end='')
+                counter += 1
+    check_double_yara(path)
 
 def init(url, key):
     return PyMISP(url, key, False, 'json', debug=False)
@@ -87,22 +111,27 @@ def echo_server(address):
 
 
 if __name__ == '__main__':
-    
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s %(message)s', datefmt='%a, %d %b %Y %H:%M:%S', filename='program.log', level=logging.DEBUG)
 
     #ssl_soc = echo_server((socket.gethostbyname('localhost'), 8089))
     misp = init(misp_url, misp_key)
     result_ip_src= misp.get_all_attributes_txt(type_attr="ip-src")
     result_ip_dest = misp.get_all_attributes_txt(type_attr="ip-dst")
     result_yara= misp.get_all_attributes_txt(type_attr="yara")
+    if not os.path.exists(os.getcwd()+"/yara_files"):
+        os.makedirs(os.getcwd()+"/yara_files")
     path = os.getcwd()+"/yara_files/yara_file.yara"
-    print(path)
+
+    #print(result_yara.text)
+    #path = "test.yara"
     file_new = open(path, "w")
     
     file_new.write(result_yara.text)
     file_new.close()
-    check_yara(path)
-    hh = yara.load(os.getcwd()+"/yara_files/saved_file.yara")
-    print(f"END {hh}")
+    check_wrong_yara(path)
+    #check_double_yara(path)
+    #hh = yara.load(os.getcwd()+"/yara_files/saved_file.yara")
+    #print(f"END {hh}")
     #print("SRC : ", result_ip_src.text, "DEST : ", result_ip_dest.text,  "YARA : ", result_yara.text)
 
     
